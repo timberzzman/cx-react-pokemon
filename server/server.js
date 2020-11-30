b3f493f9dd1923d9251b585c76eb8ece08c2e19b
Damokles <damokless@protonmail.com>
2020-11-30 14:10:15 +0100

    Adding express code to server.js

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(process.argv[2], () => {
+    console.log(`Server localhost listening on port ${process.argv[2]}`)
+  })
