fc293f798f338302c178ed0c7e281646ee223018
Damokles <damokless@protonmail.com>
2020-12-06 12:03:34 +0100

    Update post button
    la balise <a> était dans le bouton du coup il fallait appuyer sur le texte .. j'ai inversé les balises pour que la balise <a> en globe le button.

--- client/src/components/postButton.js
+++ client/src/components/postButton.js
@@ -4,9 +4,11 @@ import '../tailwind.output.css'
 
 function postButton() {
   return (
-      <button className="absolute top-5 right-16 px-2 py-1 outline-none shadow-md border-solid rounded-full hover:shadow-inner">
-        <a href="http://localhost:3000/pokemon/create">Add a pokemon</a>
-      </button>
+        <a href="http://localhost:3000/pokemon/create">
+            <button className="absolute top-5 right-16 px-2 py-1 outline-none shadow-md border-solid rounded-full hover:shadow-inner">
+                Add a pokemon
+            </button>
+        </a>
   )
 }
 
