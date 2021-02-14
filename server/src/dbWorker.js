b4ab1e57cff04831a4cc04497443f5cdc09e8724
Elouan MAILLY <leloupandco@gmail.com>
2021-02-14 19:24:20 +0100

    Adding environment variables in backend

--- server/src/dbWorker.js
+++ server/src/dbWorker.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    // port: 5433,
-    user: 'postgres',
-    password: 'root',
-    database: 'pokedex',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    ssl: true,
   },
 });
 
